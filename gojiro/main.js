91dda9276c624e82b848f28b8a961376c4f43158
Shiho317 <ex3jsb.geneg@gmail.com>
2021-08-24 07:31:31 -0700

    add body

---
+++ gojiro/main.js
@@ -0,0 +1,15 @@
+$('.slider').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed:1000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+  dots: true,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      pauseOnDotsHover: false,
+  });
